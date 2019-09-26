5aaa8d4bb666d58fc84b04b0e82dc9b8f35d48e9
Ryan Morelock <RyanMorelock@cu-biotgw-4-10.219.135.130.int.colorado.edu>
2019-09-25 22:34:14 -0600

    Changed testing of NoneType as input arg

--- test_math_lib.py
+++ test_math_lib.py
@@ -5,10 +5,12 @@ import math_lib
 class TestNone(unittest.TestCase):
 
     def test_mean_none(self):
-        self.assertEqual(math_lib.list_mean(None), None)
+        with self.assertRaises(TypeError):
+            math_lib.list_mean(None)
 
-    def test_stdev_null(self):
-        self.assertEqual(math_lib.list_stdev(None), None)
+    def test_stdev_none(self):
+        with self.assertRaises(TypeError):
+            math_lib.list_stdev(None)
 
 
 class TestString(unittest.TestCase):
