6c085a3b0ba15a689ecf298dd822e9816c4f5ba1
Ryan Morelock <RyanMorelock@cu-biotgw-4-10.219.134.144.int.colorado.edu>
2019-09-26 15:49:20 -0600

    Added functionality to read_stdin_col method and proper Null handling

--- get_data.py
+++ get_data.py
@@ -1,3 +1,18 @@
+import sys
+
 
 def read_stdin_col(col_num):
+
+    if col_num is None:
+        raise TypeError("No column provided")
+        sys.exit(1)
+
+    stdin = sys.stdin.readlines()
+
+    column = []
+
+    for line in stdin:
+        value = line.rstrip().split(' ')[col_num]
+        column.append(value)
+
     return None
