2b334be03ecb6420d920b289cf4d40fa1d6172e2
Ryan Morelock <RyanMorelock@cu-biotgw-4-10.219.134.144.int.colorado.edu>
2019-09-26 16:02:14 -0600

    Check to handle bad column types in read_stdin_col

--- test_get_data.py
+++ test_get_data.py
@@ -8,5 +8,16 @@ class TestColumnNull(unittest.TestCase):
         self.assertRaises(TypeError, get_data.read_stdin_col, None)
 
 
+class TestIncorrectValues(unittest.TestCase):
+    def test_read_stdin_col_list_values(self):
+        self.assertRaises(TypeError, get_data.read_stdin_col, ['A', 'B', 'C'])
+
+    def test_read_stdin_col_str_values(self):
+        self.assertRaises(TypeError, get_data.read_stdin_col, "A")
+
+    def test_read_stdin_col_float_values(self):
+        self.assertRaises(TypeError, get_data.read_stdin_col, 5.4)
+
+
 if __name__ == '__main__':
     unittest.main()
