6ed74828a264e705af535641379979119c1c3032
Ryan Morelock <RyanMorelock@cu-biotgw-4-10.219.135.130.int.colorado.edu>
2019-09-25 21:52:34 -0600

    Wrote the main body of code for list_mean and list_stdev and caught str exception

--- math_lib.py
+++ math_lib.py
@@ -1,6 +1,38 @@
+import sys
+import math
+
 
 def list_mean(L):
-    return None
+
+    try:
+        s = 0
+        for v in L:
+            s += v
+        return s / len(L)
+
+    except TypeError:
+        if type(L) == str:
+            raise TypeError('Cannot input string as list')
+            sys.exit(1)
+
+        if type(L) is None:
+            raise TypeError('Cannot input None as list')
+            return None
+
 
 def list_stdev(L):
-    return None
+
+    try:
+        s = 0
+        for v in L:
+            s += v
+        return math.sqrt(sum([((s/len(L))-x)**2 for x in L]) / len(L))
+
+    except TypeError:
+        if type(L) == str:
+            raise TypeError('Cannot input string as list')
+            sys.exit(1)
+
+        if type(L) is None:
+            raise TypeError('Cannot input None as list')
+            return None
