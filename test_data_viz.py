4ce0cb0544546083f766daf97d3bf594bcbf0643
Ryan Morelock <RyanMorelock@cu-biot-16-10.203.189.206.int.colorado.edu>
2019-09-26 20:33:08 -0600

    Added tests for data_viz.py histogram method

--- test_data_viz.py
+++ test_data_viz.py
@@ -39,7 +39,45 @@ class TestBoxPlot(unittest.TestCase):
 
     def test_boxplot_wrong_file(self):
         self.assertRaises(ValueError, data_viz.boxplot,
-                          [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1], "read_only.txt")
+                          [1, 2, 3, 4, 5], "read_only.txt")
+
+
+class TestHistogram(unittest.TestCase):
+    def test_histogram_null(self):
+        self.assertRaises(TypeError, data_viz.histogram, None)
+
+    def test_histogram_empty_list(self):
+        self.assertRaises(IndexError, data_viz.histogram, [])
+
+    def test_histogram_wrong_type(self):
+        self.assertRaises(TypeError, data_viz.histogram, 'test')
+        self.assertRaises(TypeError, data_viz.histogram, 3)
+        self.assertRaises(TypeError, data_viz.histogram, False)
+
+    def test_histogram_list_wrong_type(self):
+        self.assertRaises(TypeError, data_viz.histogram,
+                          ['bad', 1, ['bad']])
+
+    def test_histogram_filename_str(self):
+        self.assertRaises(TypeError, data_viz.histogram, [1, 2, 3],
+                          1)
+
+    def test_histogram_write_to_file(self):
+        data_viz.histogram([1, 2, 3], 'newfile.png')
+        self.assertTrue(os.path.exists("newfile.png"))
+        os.remove("newfile.png")
+
+    def test_histogram_permission(self):
+        with open("read_only.png", "w") as f:
+            f.write("No Access")
+        os.chmod("read_only.png", stat.S_IREAD | stat.S_IRGRP | stat.S_IROTH)
+        self.assertRaises(PermissionError, data_viz.histogram, [
+                          1, 2, 3, 4, 5], "read_only.png")
+        os.remove("read_only.png")
+
+    def test_histogram_wrong_file(self):
+        self.assertRaises(ValueError, data_viz.histogram,
+                          [1, 2, 3, 4, 5], "read_only.txt")
 
 
 if __name__ == '__main__':
